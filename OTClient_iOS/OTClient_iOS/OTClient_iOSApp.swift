b2d4e5f2d5b5b91eac172fd3e5e59a95bb685a81
Hyago Silva <hyagosilvasrs@gmail.com>
2025-08-18 20:21:08 -0300

    Initial commit Xcode project

---
+++ OTClient_iOS/OTClient_iOS/OTClient_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTClient_iOSApp.swift
+//  OTClient_iOS
+//
+//  Created by Momo on 18/08/25.
+//
+
+import SwiftUI
+
+@main
+struct OTClient_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
